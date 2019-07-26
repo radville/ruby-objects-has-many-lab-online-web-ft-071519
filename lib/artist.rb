25df9ee0653b8bc317ea1f3bffa5dcb14df43aef
Laura Radville <laura.radville@gmail.com>
2019-07-26 18:12:58 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,29 @@
+class Artist
+  
+  attr_accessor :name, :songs
+  
+  @@songs = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+  
+  def add_song(song)
+    song.artist = self
+    @songs << song
+    @@songs << song
+  end
+  
+  def add_song_by_name(song_name)
+    song = Song.new(song_name)
+    song.artist = self
+    @songs << song
+    @@songs << song
+  end
+
+  def self.song_count
+    @@songs.count
+  end
+  
+end
